84cdc2bd00a0c67ccbc7ee8905efdfb1f9abf3f5
asia <joanna.kotula@poczta.pl>
2010-03-27 15:17:22 +0100

    poprawiony auto_test - odwrotnie mówił ok i error (-;

--- skrypty/tql/auto_test/auto_test.sh
+++ skrypty/tql/auto_test/auto_test.sh
@@ -6,7 +6,7 @@ tql_prog=$1
 function do_test(){
     out=`echo $1 | cut -d. -f1`
     out=`echo $out.out`
-    $tql_prog $1 | grep "<tablet " | cut -d\" -f2 | diff -wB - $out >/dev/null
+     $tql_prog $1 | grep "<tablet " | cut -d\" -f2 | diff -wB - $out >/dev/null 
     return $?    
 }
 
@@ -14,10 +14,10 @@ errors=0
 
 for t in `ls $script_folder*.in`; do
     if do_test $t; then
+	echo "$t => OK"
+    else
 	errors=1
 	echo "$t => ERROR"
-    else
-	echo "$t => OK"
     fi
 done
 

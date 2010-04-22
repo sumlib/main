65cb666a831952a59634ee0dcce8607cefc48322
ola <ola_mogielska@o2.pl>
2010-04-22 11:33:12 +0200

    nie miescily sie odczyty

--- skrypty/tworzenie/1-baza_danych/odczyty.sql
+++ skrypty/tworzenie/1-baza_danych/odczyty.sql
@@ -4,7 +4,7 @@ CREATE TABLE Reading(
     id SERIAL,
     node1_id BIGINT NOT NULL, -- tablet_id * 100 000 + node_id
     node2_id BIGINT NOT NULL,
-    value VARCHAR(20) NOT NULL,
+    value VARCHAR(50) NOT NULL,
     type varchar(255) NOT NULL,
     CHECK (type IN ('normal', 'broken')),
     PRIMARY KEY(id)
